4a92b2ee1f9e93b28f0b5c84912f74545496207b
samandhalapradeep <pradeepsamandhala7@gmail.com>
2024-11-19 13:54:13 +0530

    terraform

--- terraform-aws-vpc/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  name = "${var.project_name}-${var.environment}"
-  az_names = slice(data.aws_availability_zones.azs.names,0,2)
-}
--- terraform-multi-envi/tfvars/provider.tf
+++ terraform-multi-envi/tfvars/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "daws76s-remote-state"
-    key    = "provisioner"
-    region = "us-east-1"
-    dynamodb_table = "daws76s-locking"
+    # bucket = "daws76s-remote-state"
+    # key    = "foreach"
+    # region = "us-east-1"
+    # dynamodb_table = "daws76s-locking"
   }
 }
 
